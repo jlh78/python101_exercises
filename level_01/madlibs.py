52ef330b249787f8a0b5771096a9cb8d117d8219
Jonah Hunter <jlh7836@gmail.com>
2025-04-20 23:14:13 -0400

    madlib - prompt the user for their name, favorite subject, and print the madlibs sentence

---
+++ level_01/madlibs.py
@@ -0,0 +1,6 @@
+sentence = "Please fill in the blanks below: " 
+print(sentence)
+
+name = input("what is your name? ")
+subject = input("what is your favorite subject? ")
+print(f"{name}'s favorite subject in school is {subject}.")
