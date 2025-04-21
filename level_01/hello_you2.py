fe12c3d63c2389515003286d3b6d81d18928c159
Jonah Hunter <jlh7836@gmail.com>
2025-04-20 22:52:31 -0400

    user info - added upper function to capitalize all letters - added len to count letters in name

---
+++ level_01/hello_you2.py
@@ -0,0 +1,4 @@
+name = input('What is your name? '.upper())
+print(f"Hello, {name}!".upper())
+character_count = len(name) 
+print(f"your name has {character_count} letters in it! awesome!".upper())
